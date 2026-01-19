9fc740b42c69c60ed19fe762a9243dd70213f6be
gregorweb <gregorweb82@gmail.com>
2026-01-20 00:22:12 +0300

    Добавление настроек jwt токена

--- server/Program.cs
+++ server/Program.cs
@@ -1,11 +1,45 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var jwtSetting = builder.Configuration.GetSection("JwtSettings");
+var secretKey = jwtSetting.GetValue<string>("SecretKey") ?? "";
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication(configure =>
+{
+    configure.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    configure.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSetting["JwtAuth"],
+        ValidAudience = jwtSetting["YourAppUsers"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+        ClockSkew = TimeSpan.Zero
+    };
+    options.Events = new JwtBearerEvents
+    {
+        OnMessageReceived = context =>
+        {
+
+            return Task.CompletedTask;
+        }
+    };
+
+});
 
 var app = builder.Build();
 
